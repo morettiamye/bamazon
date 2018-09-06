7a44fef073ea7602583c301a598435a29ceb624a
Amy Moretti <amyemoretti@gmail.com>
2018-09-05 21:42:50 -0500

    shrug emoticon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,22 +19,43 @@ connection.connect(function(err) {
 });
 
 // Welcome function for purchasing item.
-function welcome () {
+function welcome() {
     console.log("Welcome to Bamazon");
     // Display Items
+    connection.query("Select * from bamazon", function (err, results) {
+        if (err) throw err;
+        // once you have the items, prompt the user for which they'd like to bid on
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].item_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "Which item would you like to purchase?"
+                }
 
-    // Ask which item to purchase
-    inquirer
-        .prompt([
-            {
-                name: "item id",
-                type: input,
-                message: "What item would you like to purchase?"
-            },
-            {
-                name: "quantity",
-                type: input,
-                message: "How many would you like to purchase?"
-            }
-        ]).then()
-}
+            
+
+
+// function sell{
+//     // Ask which item to purchase
+//     inquirer
+//         .prompt([
+//             {
+//                 name: "item id",
+//                 type: input,
+//                 message: "What item would you like to purchase?"
+//             },
+//             {
+//                 name: "quantity",
+//                 type: input,
+//                 message: "How many would you like to purchase?"
+//             }
+//         ]).then()
+//};
