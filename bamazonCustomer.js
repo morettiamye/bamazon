e601044c0d4527ccd32598e2df616966a161bc1d
Amy Moretti <amyemoretti@gmail.com>
2018-09-06 10:18:42 -0500

    improvements

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,4 @@
-var database = require("mysql");
+var mysql= require("mysql");
 var inquirer = require("inquirer");
 
 // create the connection information for the sql database
@@ -7,55 +7,57 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "root",
-  database: "bamazon",
+  database: "bamazon_db",
   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
 });
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
+  console.log("Welcome to Bamazon");
   // run the welcome function after the connection is made to prompt the user
   welcome();
 });
 
-// Welcome function for purchasing item.
+// // Welcome function for purchasing item.
 function welcome() {
-    console.log("Welcome to Bamazon");
+    console.log("Let's shop!");
     // Display Items
-    connection.query("Select * from bamazon", function (err, results) {
+    connection.query("Select * from products", function (err, results) {
         if (err) throw err;
-        // once you have the items, prompt the user for which they'd like to bid on
-        inquirer
-            .prompt([
-                {
-                    name: "choice",
-                    type: "rawlist",
-                    choices: function () {
-                        var choiceArray = [];
-                        for (var i = 0; i < results.length; i++) {
-                            choiceArray.push(results[i].item_name);
-                        }
-                        return choiceArray;
-                    },
-                    message: "Which item would you like to purchase?"
-                }
+    });
+//         // once you have the items, prompt the user for which they'd like to bid on
+//         inquirer
+//             .prompt([
+//                 {
+//                     name: "choice",
+//                     type: "rawlist",
+//                     choices: function () {
+//                         var choiceArray = [];
+//                         for (var i = 0; i < results.length; i++) {
+//                             choiceArray.push(results[i].item_name);
+//                         }
+//                         return choiceArray;
+//                     },
+//                     message: "Which item would you like to purchase?"
+};
 
             
 
 
-// function sell{
-//     // Ask which item to purchase
-//     inquirer
-//         .prompt([
-//             {
-//                 name: "item id",
-//                 type: input,
-//                 message: "What item would you like to purchase?"
-//             },
-//             {
-//                 name: "quantity",
-//                 type: input,
-//                 message: "How many would you like to purchase?"
-//             }
-//         ]).then()
-//};
+// // function sell{
+// //     // Ask which item to purchase
+// //     inquirer
+// //         .prompt([
+// //             {
+// //                 name: "item id",
+// //                 type: input,
+// //                 message: "What item would you like to purchase?"
+// //             },
+// //             {
+// //                 name: "quantity",
+// //                 type: input,
+// //                 message: "How many would you like to purchase?"
+// //             }
+// //         ]).then()
+// //};
